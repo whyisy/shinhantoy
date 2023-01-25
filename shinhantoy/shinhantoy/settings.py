b6a1673217ab88ff66d3afdf5011457cd9345701
whyisy <cjg0504@pusan.ac.kr>
2023-01-25 18:18:48 +0900

    feat: pagination
    - add pagination per 10 pages

--- shinhantoy/shinhantoy/settings.py
+++ shinhantoy/shinhantoy/settings.py
@@ -37,11 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'order.apps.OrderConfig'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +125,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
+
+if DEBUG:
+    CORS_ALLOW_ALL_ORIGINS = True
