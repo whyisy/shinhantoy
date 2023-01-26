9c346fa1155cced88a863a04a055c0415890bde7
whyisy <cjg0504@pusan.ac.kr>
2023-01-26 19:07:26 +0900

    feat: order detail page
    - order detail page
    - order comment list

--- shinhantoy/order/serializers.py
+++ shinhantoy/order/serializers.py
@@ -1,9 +1,40 @@
 from rest_framework import serializers
 
-from .models import Order
+from .models import Order, Comment
 
 class OrderSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Order
-        fields = ('ord_no','ord_ymd')
+        fields = '__all__'
+
+class CommentSerializer(serializers.ModelSerializer):
+    member_username = serializers.SerializerMethodField()
+    tstamp = serializers.DateTimeField(
+        read_only=True, format='%Y-%m-%d %H:%M:%S'
+    )
+
+    def get_member_username(self, obj):
+        return obj.member.username
+
+    
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+
+
+class CommentCreateSerializer(serializers.ModelSerializer):
+    member = serializers.HiddenField(
+        default=serializers.CurrentUserDefault(),
+        required = False
+    )
+    
+    def validate_member(self, value):
+        if not value.is_authenticated:
+            raise serializers.ValidationError('member is required')
+        return value
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
--- shinhantoy/order/views.py
+++ shinhantoy/order/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics, mixins
 
-from .models import Order
-from .serializers import OrderSerializer
+from .models import Order, Comment
+from .serializers import OrderSerializer, CommentSerializer, CommentCreateSerializer
 
 
 class OrderListView(
@@ -17,3 +17,48 @@ class OrderListView(
     
     def get(self, request, *args, **kwargs):
         return self.list(request, args, kwargs)
+
+
+class OrderDetailView(
+    mixins.RetrieveModelMixin,
+    generics.GenericAPIView
+):
+    serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        return Order.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, args, kwargs)
+
+
+class CommentListView(
+    mixins.ListModelMixin,
+    generics.GenericAPIView
+):
+    serializer_class = CommentSerializer
+    def get_queryset(self):
+        order_id = self.kwargs.get('order_id')
+        if order_id:
+            return Comment.objects.filter(order_id=order_id) \
+                .select_related('member','order')\
+                .order_by('-id')   \
+                
+        return Comment.objects.none()
+
+    
+    def get(self, request, *args, **kwargs):
+        return self.list(request, args, kwargs)
+
+
+class CommentCreateView(
+    mixins.CreateModelMixin,
+    generics.GenericAPIView
+):
+    serializer_class = CommentCreateSerializer
+ 
+    # def get_queryset(self):   #안써도 됨
+    #     return Comment.objects.all()
+
+    def post(self,request, *args, **kwargs):
+        return self.create(request, args, kwargs)
