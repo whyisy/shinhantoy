e6f9758878fc2098fb0585fa7d4369e530e300f4
whyisy <cjg0504@pusan.ac.kr>
2023-01-27 11:13:34 +0900

    feat: upload comment
    - in the order_detail.html,you can upload comment

--- shinhantoy/order/urls.py
+++ shinhantoy/order/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path("",views.OrderListView.as_view()),
-    path("/<int:pk>/",views.OrderDetailView.as_view()),
+    path("/<int:pk>",views.OrderDetailView.as_view()),
     path("/<int:order_id>/comment",views.CommentListView.as_view()),
     path("/comment",views.CommentCreateView.as_view()),
 ]
+# resfulAPI에서는 url 제일 마지막에  / 붙이면 안됨!!!
